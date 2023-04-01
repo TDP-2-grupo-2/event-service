f06d187397c3ce410ef60f0de1bfab19013f5392
agustina235 <asegura@fi.uba.ar>
2023-04-01 20:30:08 -0300

    Feat: tests to get event by id

--- test/test_events.py
+++ test/test_events.py
@@ -75,3 +75,33 @@ def test_given_a_test_that_passed_when_creating_an_event_then_it_should_not_crea
     data = response.json()
 
     assert data["detail"] == "the chosen date has passed"
+
+def test_given_an_event_when_the_event_exits_then_it_should_return_it():
+    response = create_event1()
+    data = response.json()
+    data = data['message']
+    print(data)
+    path_end = "/events/" + data['_id']['$oid']
+    print(path_end)
+    response = client.get(path_end)
+    print(response)
+    data = response.json()
+    data = data['message']
+    assert data["name"] == "Music Fest "
+    assert data["owner"] == "Agustina Segura"
+    assert data["description"] == "Musical de pop, rock y mucho más"
+    assert data["location"] == "Estadio River "
+    assert data["capacity"] == 5000
+    assert data["dateEvent"] == "2023-06-01"
+    assert data["attendance"]== 0
+    assert data["latitud"] == 8.9
+    assert data["longitud"] == 6.8
+    assert data["start"]=="19:00:00"
+    assert data["end"]=="23:00:00"
+
+def test_given_an_event_when_the_event_does_not_exits_then_it_should_not_return_it():
+    response = client.get("/events/5428bd284042b5da2e28b6a1")
+    assert response.status_code == status.HTTP_404_NOT_FOUND, response.text
+    data = response.json()
+
+    assert data["detail"] == "The event does not exists"
