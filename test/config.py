9b8c5a6c1d7aeec183a8c98529a4d012202f8707
agustina235 <asegura@fi.uba.ar>
2023-04-01 16:02:25 -0300

    Test: add config file

---
+++ test/config.py
@@ -0,0 +1,19 @@
+from pymongo import MongoClient
+import sys
+sys.path.append("../")
+from event_service.app import app
+from event_service.databases import events_database
+
+
+def init_db():
+    client = MongoClient( 'mongodb+srv://events:events@eventscluster.hgv9toh.mongodb.net/?retryWrites=true&w=majority', 8000)
+    db_test = client['events']
+    def override_get_db():
+        try:
+            db = db_test
+            yield db
+        finally:
+            db
+
+    app.dependency_overrides[events_database.get_mongo_db] = override_get_db
+    return db_test
