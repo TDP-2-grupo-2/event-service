831a21bc6ef3aa5662fb877eb8ae3338e603a336
agustina <asegura@fi.uba.ar>
2023-05-14 20:02:12 -0300

    Feat: delete report data

--- event_service/routes/routes_config.py
+++ event_service/routes/routes_config.py
@@ -1,9 +1,10 @@
 from fastapi import APIRouter, Depends, HTTPException, status
-from event_service.databases import attende_repository, event_repository, users_schema, users_database
+from event_service.databases import attende_repository, event_repository, reports_repository
 from event_service.databases.events_database import get_mongo_db
+from event_service.databases.reports_database import get_reports_db
 from event_service.exceptions import exceptions
 from sqlalchemy.orm import Session
-from event_service.utils import jwt_handler
+
 
 config_router = APIRouter()
 
@@ -12,5 +13,9 @@ async def delete_events(events_db=Depends(get_mongo_db)):
     event_repository.delete_all_data(events_db)
 
 @config_router.delete("/databases/users", status_code=status.HTTP_200_OK)
-async def delete_attendees():
-    users_database.delete_all_data()
+async def delete_attendees(reports_db: Session = Depends(get_reports_db)):
+    reports_repository.delete_all_data(reports_db)
+
+@config_router.delete("/databases/reports",status_code=status.HTTP_200_OK)
+async def delete_events(events_db=Depends(get_mongo_db)):
+    event_repository.delete_all_data(events_db)
